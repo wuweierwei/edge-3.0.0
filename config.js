dc1584403922cd3fc678e7e4f629db9bd8b521b4
Ander <81839603@qq.com>
2016-03-22 16:17:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var config={
   "path":"F:/web/website/src",    //源码路径
   "cssGzip":false,
   "jsGzip":true,
-  "openMerge":true,               //是否打开文件合并功能（配合目录下的merge.json用）。因为一般用不上，而且多文件操作比较耗性能，故加上该配置
+  "openMerge":false,               //是否打开文件合并功能（配合目录下的merge.json用）。因为一般用不上，而且多文件操作比较耗性能，故加上该配置
   "charset":"utf8"
 }
 module.exports=config;
